d0dfc08f046807625c375d0396202765643b688d
superhvarn <56454934+superhvarn@users.noreply.github.com>
2019-12-20 15:46:41 -0500

    Update Autonomous.java

--- Autonomous.java
+++ Autonomous.java
@@ -1,5 +1,5 @@
-// Are you sure that ticks_per_rev should be used in the variable declaration for ticks_per_inches?
 package org.firstinspires.ftc.teamcode;
+
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -8,41 +8,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-/*
- *in ticks
- *1440 ticks per 360 deg rotation
- *motor.setTargetPosition(ticks);
- *motors used: mechnum
- */
-
-/*
- * Syborgs 10696 Autonomous Code
- * Purpose: create the route for the robot during the 30-second Autonomous period
- * Authors: Emily Goldman, Harish Varadarajan and Tony Zheng Yu
- * Iteration 2: Basic Routing with Encoders (Computer Vision coming soon)
- */
-
 public class Autonomous extends LinearOpMode {
-    /*
-    hardware to map:
-    * DcMotor - Front Left, Front Right, Rear Left, Rear Right
-    * DcMotor - Left Intake and Right Intake (unknown status)
-    * ARM - DcMotor Elevator, CRServo Drop Intake and Clamp
-    */
     private ElapsedTime runtime = new ElapsedTime();
     // declare motors for mapping
     private static DcMotor frontLeftMotor, frontRightMotor, rearLeftMotor, rearRightMotor;
-    //    private static DcMotor LIntake, RIntake;
-//    private static DcMotor elevator;
     private static CRServo dropIntake, clamp;
     private static Servo LHook, RHook;
     //encoder values
     private static final int wheelRadius = 3, length = 18, width = 18, ticksPerRev = 1440;
     private static final double ticksPerInches = (ticksPerRev)/(2 * Math.PI * wheelRadius), turnRadius = Math.sqrt(Math.pow((double) length / 2, 2) + Math.pow((double) width / 2,2 ));
     private static final double circumference = (2 * Math.PI * turnRadius);
-
     private void drive (double power, int inchOrDeg, String isInchOrDeg) {
-        //drive forward/backward (negative inches is driving backward)
+        /* drive forward/backward (negative inches is driving backward) */
         if (isInchOrDeg.equals("inch")) {
             frontLeftMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
             rearLeftMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
@@ -55,8 +32,8 @@ public class Autonomous extends LinearOpMode {
             while (frontLeftMotor.isBusy() || frontRightMotor.isBusy() || rearLeftMotor.isBusy() || rearRightMotor.isBusy());
             frontLeftMotor.setPower(0);
             frontRightMotor.setPower(0);
-            rearRightMotor.setPower(0);
             rearLeftMotor.setPower(0);
+            rearRightMotor.setPower(0);
         }
         else if (isInchOrDeg.equals("leftDegree")){
             frontLeftMotor.setTargetPosition((-inchOrDeg/360) * ((int) circumference));
@@ -71,8 +48,8 @@ public class Autonomous extends LinearOpMode {
             while (frontLeftMotor.isBusy() || frontRightMotor.isBusy() || rearLeftMotor.isBusy() || rearRightMotor.isBusy());
             frontLeftMotor.setPower(0);
             frontRightMotor.setPower(0);
-            rearRightMotor.setPower(0);
             rearLeftMotor.setPower(0);
+            rearRightMotor.setPower(0);
         }
         else if (isInchOrDeg.equals("rightDegree")){
             frontLeftMotor.setTargetPosition((inchOrDeg/360) * ((int) circumference));
@@ -87,25 +64,25 @@ public class Autonomous extends LinearOpMode {
             while (frontLeftMotor.isBusy() || frontRightMotor.isBusy() || rearLeftMotor.isBusy() || rearRightMotor.isBusy());
             frontLeftMotor.setPower(0);
             frontRightMotor.setPower(0);
-            rearRightMotor.setPower(0);
             rearLeftMotor.setPower(0);
+            rearRightMotor.setPower(0);
         }
         else {
             idle();
         }
     }
     //initialize
-    @Override
-    public void runOpMode() {
-        // map motors
+
+    public void runOpMode() throws InterruptedException  {
+        /* map motors */
         frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
         rearLeftMotor = hardwareMap.get(DcMotor.class, "rearLeft");
         rearRightMotor = hardwareMap.get(DcMotor.class, "rearRight");
         // map intakes and arm
-//        LIntake = hardwareMap.get(DcMotor.class, "leftIntake");
-//        RIntake = hardwareMap.get(DcMotor.class, "rightIntake");
-//        elevator = hardwareMap.get(DcMotor.class, "elevator");
+        DcMotor LIntake = hardwareMap.get(DcMotor.class, "leftIntake");
+        hardwareMap.get(DcMotor.class, "rightIntake");
+        DcMotor elevator = hardwareMap.get(DcMotor.class, "elevator");
         dropIntake = hardwareMap.get(CRServo.class, "dropIntake");
         clamp = hardwareMap.get(CRServo.class, "clamp");
         LHook = hardwareMap.get(Servo.class, "leftHook");
@@ -126,12 +103,14 @@ public class Autonomous extends LinearOpMode {
         frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rearLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rearRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        telemetry.addData("Status: ", "Initializing");
+
         waitForStart();
         runtime.reset();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
         while (opModeIsActive()) {
-            telemetry.addData("Status: ", "Initialized");
-            //unclamping just to be safe
+            telemetry.clearAll();
+            //reverse-clamping just to be safe
             clamp.setPower(-1);
             // driving to stone
             drive(0.6, 30, "inch");
@@ -141,7 +120,7 @@ public class Autonomous extends LinearOpMode {
             // facing alliance bridge
             drive(0.6, 90, "rightDegree");
             drive(0.6, 96, "inch");
-            // facing foundation
+            /* facing foundation */
             drive(0.6, 90, "leftDegree");
             LHook.setPosition(0.5);
             RHook.setPosition(0.5);
@@ -160,9 +139,9 @@ public class Autonomous extends LinearOpMode {
             drive(0.6, 25, "inch");
             clamp.setPower(1);
             // facing toward foundation
-            drive(0.6, 90, "rightDegree");
+            drive(0.6, 90, "rightDegrees");
             dropIntake.setPower(-0.6);
-            drive(0.6, 90, "inch");
+            drive(0.6, 121, "inch");
             // get the block on top of the foundation
             dropIntake.setPower(0.4);
             clamp.setPower(-1);
@@ -173,6 +152,9 @@ public class Autonomous extends LinearOpMode {
         }
     }
 }
+
+
+
 
 
 
