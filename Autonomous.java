ff50e12772b82d394f158b4a20b711fb1321ed5a
tzheng22 <t.ho.zheng@gmail.com>
2020-01-03 15:57:05 -0500

    Update Autonomous.java

--- Autonomous.java
+++ Autonomous.java
@@ -12,15 +12,15 @@ public class Autonomous extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     // declare motors for mapping
     private static DcMotor frontLeftMotor, frontRightMotor, rearLeftMotor, rearRightMotor;
-    private static CRServo dropIntake, clamp;
-    private static Servo LHook, RHook;
+    private static DcMotor elevator1, elevator2;
+    private static Servo LHook, RHook, clamp, arm;
     //encoder values
     private static final int wheelRadius = 3, length = 18, width = 18, ticksPerRev = 1440;
     private static final double ticksPerInches = (ticksPerRev)/(2 * Math.PI * wheelRadius), turnRadius = Math.sqrt(Math.pow((double) length / 2, 2) + Math.pow((double) width / 2,2 ));
     private static final double circumference = (2 * Math.PI * turnRadius);
     private void drive (double power, int inchOrDeg, String isInchOrDeg) {
         /* drive forward/backward (negative inches is driving backward) */
-        if (isInchOrDeg.equals("inch")) {
+        if (isInchOrDeg.equals("inchForward")) {
             frontLeftMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
             rearLeftMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
             frontRightMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
@@ -28,6 +28,21 @@ public class Autonomous extends LinearOpMode {
             frontLeftMotor.setPower(power);
             frontRightMotor.setPower(power);
             rearRightMotor.setPower(power);
+            rearLeftMotor.setPower(power);
+            while (frontLeftMotor.isBusy() || frontRightMotor.isBusy() || rearLeftMotor.isBusy() || rearRightMotor.isBusy());
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            rearLeftMotor.setPower(0);
+            rearRightMotor.setPower(0);
+        }
+        if (isInchOrDeg.equals("strafe")) {
+            frontLeftMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
+            rearLeftMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
+            frontRightMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
+            rearRightMotor.setTargetPosition((int) ticksPerInches * inchOrDeg);
+            frontLeftMotor.setPower(-power);
+            frontRightMotor.setPower(power);
+            rearRightMotor.setPower(-power);
             rearLeftMotor.setPower(power);
             while (frontLeftMotor.isBusy() || frontRightMotor.isBusy() || rearLeftMotor.isBusy() || rearRightMotor.isBusy());
             frontLeftMotor.setPower(0);
@@ -71,6 +86,72 @@ public class Autonomous extends LinearOpMode {
             idle();
         }
     }
+    private void Red1() {
+        //unclamp - just to be safe - and then drive toward and grab a block
+        clamp.setPosition(1);
+        drive(0.6, 12, "inchForward");
+        clamp.setPosition(0.15);
+        drive(-0.6, 16, "inchForward");
+        drive(1, 90, "leftDegree");
+        //cross alliance bridge and drop block onto foundation (assuming other team moved foundation to parking zone)
+        drive(0.6, 54, "inchForward");
+        elevator1.setPower(0.3);
+        clamp.setPosition(0.8);
+        //park under alliance bridge
+        drive(-0.6, 26, "inchForward");
+        idle();
+    }
+    private void Red2() {
+        //unclamp - just to be safe - and then drive toward and grab the foundation
+        clamp.setPosition(0.8);
+        LHook.setPosition(0.5);
+        RHook.setPosition(0.5);
+        drive(0.6, 12, "inchForward");
+        LHook.setPosition(1);
+        RHook.setPosition(1);
+        //move foundation into parking zone and park under alliance bridge
+        drive(0.6, 6, "strafe");
+        drive(1, 90, "turnLeft");
+        drive(-0.6, 14, "strafe");
+        drive(0.6, 20, "inchForward");
+        LHook.setPosition(0.5);
+        RHook.setPosition(0.5);
+        drive(-0.6, 44, "inchForward");
+        idle();
+    }
+    private void Blue1() {
+        //unclamp - just to be safe - and then drive toward and grab a block
+        clamp.setPosition(0.8);
+        drive(0.6, 12, "inchForward");
+        clamp.setPosition(0.15);
+        drive(-0.6, 16, "inchForward");
+        drive(1, 90, "rightDegree");
+        //cross alliance bridge and drop block onto foundation (assuming other team moved foundation to parking zone)
+        drive(0.6, 54, "inchForward");
+        elevator1.setPower(0.3);
+        clamp.setPosition(0.8);
+        //park under alliance bridge
+        drive(-0.6, 26, "inchForward");
+        idle();
+    }
+    private void Blue2() {
+        //unclamp - just to be safe - and then drive toward and grab the foundation
+        clamp.setPosition(0.8);
+        LHook.setPosition(0.5);
+        RHook.setPosition(0.5);
+        drive(0.6, 12, "inchForward");
+        LHook.setPosition(1);
+        RHook.setPosition(1);
+        //move foundation into parking zone and park under alliance bridge
+        drive(-0.6, 6, "strafe");
+        drive(1, 90, "turnRight");
+        drive(0.6, 14, "strafe");
+        drive(0.6, 20, "inchForward");
+        LHook.setPosition(0.5);
+        RHook.setPosition(0.5);
+        drive(-0.6, 44, "inchForward");
+        idle();
+    }
     //initialize
 
     public void runOpMode() throws InterruptedException  {
@@ -79,12 +160,11 @@ public class Autonomous extends LinearOpMode {
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
         rearLeftMotor = hardwareMap.get(DcMotor.class, "rearLeft");
         rearRightMotor = hardwareMap.get(DcMotor.class, "rearRight");
-        // map intakes and arm
-        DcMotor LIntake = hardwareMap.get(DcMotor.class, "leftIntake");
-        hardwareMap.get(DcMotor.class, "rightIntake");
-        DcMotor elevator = hardwareMap.get(DcMotor.class, "elevator");
-        dropIntake = hardwareMap.get(CRServo.class, "dropIntake");
-        clamp = hardwareMap.get(CRServo.class, "clamp");
+        // map elevators and arm
+        elevator1 = hardwareMap.get(DcMotor.class, "elevator1");
+        elevator2 = hardwareMap.get(DcMotor.class, "elevator2");
+        arm = hardwareMap.get(Servo.class, "arm");
+        clamp = hardwareMap.get(Servo.class, "clamp");
         LHook = hardwareMap.get(Servo.class, "leftHook");
         RHook = hardwareMap.get(Servo.class, "rightHook");
 
@@ -92,6 +172,8 @@ public class Autonomous extends LinearOpMode {
         rearLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rearRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        elevator1.setDirection(DcMotorSimple.Direction.REVERSE);
+        elevator2.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // run encoders for motors
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -110,45 +192,7 @@ public class Autonomous extends LinearOpMode {
         telemetry.update();
         while (opModeIsActive()) {
             telemetry.clearAll();
-            //reverse-clamping just to be safe
-            clamp.setPower(-1);
-            // driving to stone
-            drive(0.6, 30, "inch");
-            // getting the stone
-            dropIntake.setPower(-0.5);
-            clamp.setPower(1);
-            // facing alliance bridge
-            drive(0.6, 90, "rightDegree");
-            drive(0.6, 96, "inch");
-            /* facing foundation */
-            drive(0.6, 90, "leftDegree");
-            LHook.setPosition(0.5);
-            RHook.setPosition(0.5);
-            drive(-0.6, -4, "inch");
-            // facing toward landing zone
-            drive(0.6, 90, "rightDegrees");
-            drive(0.6, 9, "inch");
-            //unhooking and placing block on foundation
-            LHook.setPosition(0);
-            RHook.setPosition(0);
-            clamp.setPower(-1);
-            // crossing alliance bridge
-            drive(-0.6, -90, "inch");
-            drive(0.6, 90, "leftDegree");
-            //getting the second block
-            drive(0.6, 25, "inch");
-            clamp.setPower(1);
-            // facing toward foundation
-            drive(0.6, 90, "rightDegrees");
-            dropIntake.setPower(-0.6);
-            drive(0.6, 121, "inch");
-            // get the block on top of the foundation
-            dropIntake.setPower(0.4);
-            clamp.setPower(-1);
-            dropIntake.setPower(-0.4);
-            //park under alliance bridge
-            drive(-0.6, -65, "inch");
-            idle();
+
         }
     }
 }
