6dbe9584ac5b9dab6141ad590069bbb924d146c1
Rohan Ghotra <38005466+Rohan75@users.noreply.github.com>
2020-01-26 14:49:53 -0500

    Added imu angle update

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -2,6 +2,7 @@
 public class FieldCentricDrive extends LinearOpMode {
 
   DcMotor FL, FR, BL, BR;
+  BNO055IMU imu;
   
   @Override
   public void runOpMode() throws InterruptedException {
@@ -19,11 +20,35 @@ public class FieldCentricDrive extends LinearOpMode {
     BL.setZeroPowerBehaviour(DcMotor.ZeroPowerBehavior.BRAKE);
     BR.setZeroPowerBehaviour(DcMotor.ZeroPowerBehavior.BRAKE);
     
+    imu = hardwareMap.get(BNO055IMU.class, "imu");
+    
+    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+    parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+    parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+    parameters.loggingEnabled = true;
+    parameters.loggingTag = "IMU";
+    
+    imu.initialize(parameters);
+    
+    Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    telemetry.addAction(new Runnable() {
+      @Override public void run() {
+        // Acquiring the angles is relatively expensive; we don't want
+        // to do that in each of the three items that need that info, as that's
+        // three times the necessary expense.
+        angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        }
+      });
+    double angle = 0;
+    
     waitForStart();
     
-    double angle;
-    
     while (opModeIsActive()) {
+      
+      telemetry.addLine("angle: ", angle);
+      telemetry.update();
+      
+      angle = angles.secondAngle;
       
       double LY = -gamepad1.left_stick_y * Math.cos(Math.toRadians(angle));
       double LX = gamepad1.left_stick_x * Math.sin(Math.toRadians(angle));
