1d7326012a07075128ee967dc67895419ff7fb83
Rohan Ghotra <38005466+Rohan75@users.noreply.github.com>
2020-01-26 12:33:47 -0500

    Implemented trig-based FCD

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -11,8 +11,8 @@ public class FieldCentricDrive extends LinearOpMode {
     BL = hardwareMap.get(DcMotor.class, "BL");
     BR = hardwareMap.get(DcMotor.class, "BR");
     
-    FR.setDirection(DcMotor.Direction.REVERSE);
-    BR.setDirection(DcMotor.Direction.REVERSE);
+    FL.setDirection(DcMotor.Direction.REVERSE);
+    BL.setDirection(DcMotor.Direction.REVERSE);
     
     FL.setZeroPowerBehaviour(DcMotor.ZeroPowerBehavior.BRAKE);
     FR.setZeroPowerBehaviour(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -21,10 +21,12 @@ public class FieldCentricDrive extends LinearOpMode {
     
     waitForStart();
     
+    double angle;
+    
     while (opModeIsActive()) {
       
-      double LY = -gamepad1.left_stick_y;
-      double LX = gamepad1.left_stick_x;
+      double LY = -gamepad1.left_stick_y * Math.cos(Math.toRadians(angle));
+      double LX = gamepad1.left_stick_x * Mathsin(Math.toRadians(angle));
       double RX = gamepad1.right_stick_x;
       
       FL.setPower(LY + LX + RX);
