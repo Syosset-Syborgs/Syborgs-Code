ae1893b556c0eb59829ec2b8aa1b0dc60be58b2d
Rohan Ghotra <38005466+Rohan75@users.noreply.github.com>
2020-01-28 19:11:36 -0500

    Update DistAuto.java

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistAuto.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistAuto.java
@@ -1,1 +1,17 @@
 
+@Autonomous(name="Blue")
+public class DistAuto {
+  
+  DcMotor FL, FR, BL, BR;
+  CRServo claw;
+  ModernRoboticsI2cRangeSensor;
+  BNO055IMU imu;
+  
+  @Override
+  public void runOpMode() throws InterruptedException {
+    
+    FL = hardwareMap.get(DcMotor.class, 
+    
+  }
+  
+}
