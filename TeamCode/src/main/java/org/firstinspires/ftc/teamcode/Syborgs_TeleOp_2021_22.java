038de55e5cdd4583216f795fed4b0b37041064b3
tzheng22 <t.ho.zheng@gmail.com>
2021-12-11 03:33:36 -0500

    added ftc freight frenzy

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Syborgs_TeleOp_2021_22.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Syborgs_TeleOp_2021_22 extends OpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor FrontLeft, FrontRight, RearLeft, RearRight, arm;
+    private CRServo intake;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        FrontLeft = hardwareMap.get(DcMotor.class, "");
+        FrontRight = hardwareMap.get(DcMotor.class, "");
+        RearLeft = hardwareMap.get(DcMotor.class, "");
+        RearRight = hardwareMap.get(DcMotor.class, "");
+        arm = hardwareMap.get(DcMotor.class, "");
+        intake = hardwareMap.get(CRServo.class, "");
+
+        FrontRight.setDirection(DcMotor.Direction.REVERSE);
+        RearRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        FrontLeft.setPower(gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
+        RearLeft.setPower(gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
+        FrontRight.setPower(gamepad1.right_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
+        RearRight.setPower(gamepad1.right_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
+
+        if (gamepad1.dpad_up) {
+            arm.setPower(0.4);
+        } else if (gamepad1.dpad_down) {
+            arm.setPower(-0.4);
+        }
+
+        if (gamepad1.right_bumper) {
+            intake.setPower(0.4);
+        }
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
