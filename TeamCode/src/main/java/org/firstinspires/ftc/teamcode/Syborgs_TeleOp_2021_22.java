8e5c0ea00cc36cfc4001189b5549e0e028579f53
tzheng22 <t.ho.zheng@gmail.com>
2022-01-11 09:49:34 -0500

    Update Syborgs_TeleOp_2021_22.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Syborgs_TeleOp_2021_22.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Syborgs_TeleOp_2021_22.java
@@ -1,38 +1,39 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.CRServo;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.CRServo;
 
-public class Syborgs_TeleOp_2021_22 extends OpMode {
+@TeleOp(name="Syborgs TeleOp", group="")
+public class Syborgs_TeleOp_2022 extends OpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor FrontLeft, FrontRight, RearLeft, RearRight, arm;
-    private CRServo intake;
+    private DcMotor FL, FR, RL, RR, arm, carousel;
+    private CRServo intake, hand;
 
-    @Override
     public void init() {
-        telemetry.addData("Status", "Initialized");
+        FL = hardwareMap.get(DcMotor.class, "Front Left");
+        FR = hardwareMap.get(DcMotor.class, "Front Right");
+        RL = hardwareMap.get(DcMotor.class, "Rear Left");
+        RR = hardwareMap.get(DcMotor.class, "Rear Right");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        carousel = hardwareMap.get(DcMotor.class, "spin");
+        intake = hardwareMap.get(CRServo.class, "intake");
+        hand = hardwareMap.get(CRServo.class, "hand");
 
-        FrontLeft = hardwareMap.get(DcMotor.class, "");
-        FrontRight = hardwareMap.get(DcMotor.class, "");
-        RearLeft = hardwareMap.get(DcMotor.class, "");
-        RearRight = hardwareMap.get(DcMotor.class, "");
-        arm = hardwareMap.get(DcMotor.class, "");
-        intake = hardwareMap.get(CRServo.class, "");
+        FR.setDirection(DcMotor.Direction.REVERSE);
+        RR.setDirection(DcMotor.Direction.REVERSE);
 
-        FrontRight.setDirection(DcMotor.Direction.REVERSE);
-        RearRight.setDirection(DcMotor.Direction.REVERSE);
+        telemetry.addData("Status", "Initialised");
     }
 
-    @Override
     public void loop() {
-        FrontLeft.setPower(gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
-        RearLeft.setPower(gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
-        FrontRight.setPower(gamepad1.right_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
-        RearRight.setPower(gamepad1.right_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
+        FL.setPower(gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
+        RL.setPower(gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
+        FR.setPower(gamepad1.right_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
+        RR.setPower(gamepad1.right_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
 
         if (gamepad1.dpad_up) {
             arm.setPower(0.4);
@@ -49,10 +50,5 @@ public class Syborgs_TeleOp_2021_22 extends OpMode {
         if (gamepad2.a) {
             carousel.setPower(0.7);
         }
-    }
-
-    @Override
-    public void stop() {
-
     }
 }
